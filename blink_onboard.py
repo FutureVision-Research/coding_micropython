01ef920dd5a86fa0e14a4a19d3660b963e39f421
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2024-08-16 09:44:12 -0700

    Update blink_onboard.py

--- blink_onboard.py
+++ blink_onboard.py
@@ -1,10 +1,13 @@
-import machine
-import utime
+#This script blinks the onboard LED (GPIO pin 25) on a Raspberry Pi Pico RP2040.
+#Be sure to place a 51Ω (or higher) resistor in series with the LED.
 
-led_onboard = machine.Pin(25, machine.Pin.OUT)
+import machine #Loads machine library, which allows us to control GPIO pins
+import utime   #Loads the microtime library, which allows us to keep track of time
 
-while True:
-  led_onboard.value(1)
-  utime.sleep(5)
-  led_onboard.value(0)
-  utime.sleep(5)
+led_onboard = machine.Pin(25, machine.Pin.OUT) #Sets GPIO pin 25 as an output and assigns it to an object called "led_onboard"
+
+while True:  #This creates a continous loop
+  led_onboard.value(1)  #Sets the pin high, which turns the LED on
+  utime.sleep(5)        #Waits five seconds
+  led_onboard.value(0)  #Sets the pin low, which turns the LED off
+  utime.sleep(5)        #Waits five seconds
