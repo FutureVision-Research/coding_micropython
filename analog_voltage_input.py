556c27b5fbdc16cff5428b9eaf95c0051bd4dedc
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2024-10-23 21:11:13 -0700

    Update analog_voltage_input.py

--- analog_voltage_input.py
+++ analog_voltage_input.py
@@ -1,4 +1,8 @@
 # Reads the analog input of a potentiometer connected to GP26 and converts that value to a voltage
+# Keep in mind:
+# The ADC in your Pico has a resolution of 12 bits, meaning that it can transform an analog signal into a digital signal as a number ranging from 0 to 4095.
+# However, this is transformed inside MicroPython to a 16-bit range, which is 0 to 65535. This is because other MicroPython controllers have a 16 bit ADC.
+# That means when we read an analog value, we'll receive a number between 0 and 65535.
 
 from machine import ADC, Pin
 import utime
@@ -10,7 +14,7 @@ adc_pin = ADC(Pin(26))
 VREF = 3.3
 
 while True:
-    # Read raw ADC value (12-bit: 0-4095)
+    # Read raw ADC value
     raw_value = adc_pin.read_u16()  # Returns a value between 0 and 65535 in MicroPython (16-bit)
 
     # Convert raw value to voltage
