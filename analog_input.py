8289251c4955aa65ff20b281cb67b9f255046e07
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2024-10-23 21:06:11 -0700

    Update analog_input.py

--- analog_input.py
+++ analog_input.py
@@ -1,4 +1,8 @@
-#Reads the analog input from a potentiomter connected to an analog input and outputs the result to the shell
+# Reads the analog input from a potentiomter connected to an analog input and outputs the result to the shell
+# Keep in mind:
+# The ADC in your Pico has a resolution of 12 bits, meaning that it can transform an analog signal into a digital signal as a number ranging from 0 to 4095.
+# However, this is transformed inside MicroPython to a 16-bit range, which is 0 to 65535. This is because other MicroPython controllers have a 16 bit ADC.
+# Therefore, when we read an analog value, we'll receive a number between 0 and 65535.
 
 from machine import ADC, Pin #Loads the classes ADC and Pin from the library module.
 import utime
@@ -7,7 +11,8 @@ import utime
 adc_pin = ADC(Pin(26))
 
 while True:
-    # Read raw ADC value (16-bit: 0-65535)
+    # Read raw ADC value
+    
     raw_value = adc_pin.read_u16()  # Returns a value between 0 and 65535 in MicroPython
 
     # Display the raw ADC value
