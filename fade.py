84bcf819806a8edc1e8aabeeb5d3cdd589bd9708
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2024-10-31 17:39:28 -0700

    Update fade.py

--- fade.py
+++ fade.py
@@ -1,4 +1,6 @@
 # This script causes an LED to fade off and on in a loop
+# Regarding the "Fade up" range: The upper limit 65536 is set because the range function in Python (and MicroPython) is non-inclusive of the upper limit. 
+# This means the loop will iterate up to, but not including, 65536.
 
 from machine import Pin, PWM
 import time
@@ -8,11 +10,12 @@ led = PWM(Pin(15))
 led.freq(1000)  # Set the PWM frequency to 1kHz
 
 while True:
-    for duty in range(0, 65536, 256):  # Increment duty cycle by 256
+    # Fade up
+    for duty in range(0, 65536, 256):  # Increment duty cycle by 256. 
         led.duty_u16(duty)  # Set PWM duty cycle (16-bit)
         time.sleep(0.01)    # Small delay for smooth fading
 
-    # Fade out
+    # Fade down
     for duty in range(65535, -1, -256):  # Decrement duty cycle by 256
         led.duty_u16(duty)
         time.sleep(0.01)
