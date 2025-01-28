14fcbfb4a898c87828746887223a08931ca9fd36
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2025-01-28 15:48:20 -0800

    Update pir_motion.py

--- section2/pir_motion.py
+++ section2/pir_motion.py
@@ -1,6 +1,7 @@
 # This script is intended for use with a PIR motion sensor.
 # When the input is high, it sets the output pin high, and vice versa
 from machine import Pin
+from utime import sleep
 
 # Define GPIO pins
 input_pin = Pin(16, Pin.IN, None)  # GPIO 16 as input. The PIR sensor will hold its output low until an event is triggered so we don't want to use internal pull down. 
@@ -8,7 +9,7 @@ output_pin = Pin(17, Pin.OUT) # GPIO 17 as output
 
 # Main loop
 while True:
-    if input_pin.value():  # Check if GPIO 16 is high
+    if input_pin.value()==1:  # Check if GPIO 16 is high
         output_pin.value(1)  # Set GPIO 17 high
     else:
         output_pin.value(0)  # Set GPIO 17 low
