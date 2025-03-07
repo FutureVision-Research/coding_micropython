eea728bf598762dc30325c71b1e00d1c48e169cd
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2025-03-07 08:55:50 -0800

    Add files via upload

---
+++ section3/servo_sweep.py
@@ -0,0 +1,22 @@
+# This script demonstrates sweeping a servo back and forth
+# Written by Benjamin Book
+
+from machine import Pin, PWM
+from utime import sleep
+
+servo = PWM(Pin(19))
+servo.freq(50)
+servo.duty_u16(1500)
+try:
+    while True:
+       for duty in range(1500, 7850, 10):
+           servo.duty_u16(duty)
+           print(f'The PWM frequency is {duty}')
+           sleep(0.01)
+       for duty in range(7850, 1500, -10):
+           servo.duty_u16(duty)
+           print(f'The PWM frequency is {duty}')
+           sleep(0.01)
+except KeyboardInterrupt:
+    servo.deinit()
+    print('Servo is disabled')
