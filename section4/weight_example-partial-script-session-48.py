fcca0e25dcce1444305592543680b52c6381d516
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2025-05-08 10:14:32 -0700

    Add files via upload

---
+++ section4/weight_example-partial-script-session-48.py
@@ -0,0 +1,8 @@
+weight_earth = float(input("Enter your weight on Earth (in pounds): "))
+
+factor_moon = 0.165
+factor_mars = 0.378
+factor_jupiter = 2.528
+
+weight_moon = weight_earth * factor_moon
+weight_mars = weight_earth * factor_mars
